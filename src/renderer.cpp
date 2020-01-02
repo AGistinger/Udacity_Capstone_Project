7b55d8ded02062cf910ba2efbf68babe5ab22a5d
Adrianne <54363707+Nyrathalnian@users.noreply.github.com>
2020-01-02 09:10:03 -0500

    Updated renderer.h and .cpp updated cmakelists
    Added function to add a background image and updated cmakelists to allow for sdl2_image library

---
+++ src/renderer.cpp
@@ -0,0 +1,110 @@
+#include "renderer.h"
+#include <iostream>
+#include <string>
+
+Renderer::Renderer(const std::size_t screen_width,
+                   const std::size_t screen_height,
+                   const std::size_t grid_width, const std::size_t grid_height)
+    : screen_width(screen_width),
+      screen_height(screen_height),
+      grid_width(grid_width),
+      grid_height(grid_height) {
+  // Initialize SDL
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    std::cerr << "SDL could not initialize.\n";
+    std::cerr << "SDL_Error: " << SDL_GetError() << "\n";
+  }
+        
+  //Initialize background texture
+  if(background == NULL)
+  {
+    background = load_image("coral_bckgrnd.png", sdl_renderer);
+  }
+
+  // Create Window
+  sdl_window = SDL_CreateWindow("Snake Game", SDL_WINDOWPOS_CENTERED,
+                                SDL_WINDOWPOS_CENTERED, screen_width,
+                                screen_height, SDL_WINDOW_SHOWN);
+
+  if (nullptr == sdl_window) {
+    std::cerr << "Window could not be created.\n";
+    std::cerr << " SDL_Error: " << SDL_GetError() << "\n";
+  }
+
+  // Create renderer
+  sdl_renderer = SDL_CreateRenderer(sdl_window, -1, SDL_RENDERER_ACCELERATED);
+  if (nullptr == sdl_renderer) {
+    std::cerr << "Renderer could not be created.\n";
+    std::cerr << "SDL_Error: " << SDL_GetError() << "\n";
+  }
+}
+
+Renderer::~Renderer() {
+  SDL_DestroyWindow(sdl_window);
+  SDL_Quit();
+}
+
+void Renderer::Render(Snake const snake, SDL_Point const &food) {
+  SDL_Rect block;
+  block.w = screen_width / grid_width;
+  block.h = screen_height / grid_height;
+
+  //SDL_SetRenderDrawColor(sdl_renderer, 0x1E, 0x1E, 0x1E, 0xFF);
+  
+  //RenderCopy for background image
+  SDL_RenderCopy(sdl_renderer, background, NULL, NULL);
+  
+  //Clear screen
+  SDL_RenderClear(sdl_renderer);
+
+  // Render food
+  SDL_SetRenderDrawColor(sdl_renderer, 0xFF, 0xCC, 0x00, 0xFF);
+  block.x = food.x * block.w;
+  block.y = food.y * block.h;
+  SDL_RenderFillRect(sdl_renderer, &block);
+
+  // Render snake's body
+  SDL_SetRenderDrawColor(sdl_renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+  for (SDL_Point const &point : snake.body) {
+    block.x = point.x * block.w;
+    block.y = point.y * block.h;
+    SDL_RenderFillRect(sdl_renderer, &block);
+  }
+
+  // Render snake's head
+  block.x = static_cast<int>(snake.head_x) * block.w;
+  block.y = static_cast<int>(snake.head_y) * block.h;
+  if (snake.alive) {
+    SDL_SetRenderDrawColor(sdl_renderer, 0x00, 0x7A, 0xCC, 0xFF);
+  } else {
+    SDL_SetRenderDrawColor(sdl_renderer, 0xFF, 0x00, 0x00, 0xFF);
+  }
+  SDL_RenderFillRect(sdl_renderer, &block);
+
+  // Update Screen
+  SDL_RenderPresent(sdl_renderer);
+}
+
+void Renderer::UpdateWindowTitle(int score, int fps) {
+  std::string title{"Snake Score: " + std::to_string(score) + " FPS: " + std::to_string(fps)};
+  SDL_SetWindowTitle(sdl_window, title.c_str());
+}
+
+//Load a texture to be used for the Background
+SDL_Texture* Renderer::load_image(const std::string &img_path, SDL_Renderer* renderer)
+{
+  auto background = IMG_Load(img_path.c_str()); //creates a SDL_Surface*
+  if(background == NULL)
+  {
+    std::cout << "SDL_Surface image load failed: " << SDL_GetError() << std::endl;
+  }
+
+  SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, background); //creates a SDL_Texture from a SDL_Surface
+  if(texture == NULL)
+  {
+    std::cout << "SDL_Texure load failed: " << SDL_GetError() << std::endl;
+  }
+  
+  SDL_FreeSurface(background);  //free memory from allocated surface
+  return texture;
+}
---
+++ src/renderer.h
@@ -0,0 +1,31 @@
+#ifndef RENDERER_H
+#define RENDERER_H
+
+#include <vector>
+#include <string>
+#include "SDL.h"
+#include "snake.h"
+#include <SDL_image.h>
+
+class Renderer {
+ public:
+  Renderer(const std::size_t screen_width, const std::size_t screen_height,
+           const std::size_t grid_width, const std::size_t grid_height);
+  ~Renderer();
+
+  void Render(Snake const snake, SDL_Point const &food);
+  void UpdateWindowTitle(int score, int fps);
+
+ private:
+  SDL_Window *sdl_window;
+  SDL_Renderer *sdl_renderer;
+  SDL_Texture* load_image(const std::string &img_path, SDL_Renderer* sdl_renderer);
+
+  const std::size_t screen_width;
+  const std::size_t screen_height;
+  const std::size_t grid_width;
+  const std::size_t grid_height;
+  SDL_Texture* background;
+};
+
+#endif
